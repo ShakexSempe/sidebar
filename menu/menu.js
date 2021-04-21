7236c91ab0f95058e2e80286747b9a9645e7dd72
Ray-Shakex <bshakex@gmail.com>
2021-04-21 11:46:52 +0200

    text and box shadows

--- menu/menu.js
+++ menu/menu.js
@@ -2,65 +2,65 @@
 const menu = [
   {
     id: 1,
-    title: "buttermilk pancakes",
-    category: "breakfast",
+    title: "Frozen Theme Cake",
+    category: "girls",
     price: 45.00,
     img: "./images/item-1.jpg",
     desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
   },
   {
     id: 2,
-    title: "diner double",
-    category: "lunch",
-    price: 13.99,
+    title: "dragoballZ Theme cake",
+    category: "boys",
+    price: 45.00,
     img: "./images/item-2.jpg",
     desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
   },
   {
     id: 3,
-    title: "godzilla milkshake",
-    category: "shakes",
-    price: 6.99,
+    title: "BMW theme cake",
+    category: "boys",
+    price: 45.00,
     img: "./images/item-3.jpg",
     desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
   },
   {
     id: 4,
-    title: "country delight",
-    category: "breakfast",
-    price: 20.99,
+    title: "white wedding",
+    category: "adults",
+    price: 45.00,
     img: "./images/item-4.jpg",
     desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
   },
   {
     id: 5,
-    title: "egg attack",
-    category: "lunch",
-    price: 22.99,
+    title: "mother's birthday",
+    category: "adult",
+    price: 45.00,
     img: "./images/item-5.jpg",
     desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
   },
   {
     id: 6,
-    title: "oreo dream",
-    category: "shakes",
-    price: 18.99,
+    title: "mother's birthday rose bucket",
+    category: "adults",
+    price: 45.00,
     img: "./images/item-6.jpg",
     desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
   },
   {
     id: 7,
-    title: "bacon overflow",
-    category: "breakfast",
-    price: 8.99,
+    title: "ladies birthday",
+    category: "adults",
+    price: 45.00,
     img: "./images/item-7.jpg",
     desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
   },
   {
     id: 8,
-    title: "american classic",
-    category: "lunch",
-    price: 12.99,
+    title: "birthday classic drip",
+    category: "girls",
+    price: 45.00,
     img: "./images/item-8.jpg",
     desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
   },
@@ -84,16 +84,26 @@ window.addEventListener('DOMContentLoaded' , () => {
   displayMenuItems(menu)
 });
 
+
 //filter items
 filterButtons.forEach(btn => {
   btn.addEventListener('click', e => {
     const category = e.currentTarget.dataset.id;
     const menuCategory = menu.filter(menuItem => {
-      return menuItem
-    })
-    console.log(menuCategory);
-  })
-})
+
+      if (menuItem.category === category) {
+        return menuItem
+      }
+    });
+
+    if (category === 'all') {
+      displayMenuItems(menu)
+    }
+    else {
+      displayMenuItems(menuCategory);
+    }
+  });
+});
 
 
 
